c1e067c4d9db94f8573145d068963d4335c9b014
brebz7 <andrei_brebeanu@yahoo.com>
2018-05-13 19:28:34 +0300

    hackerRank string problem

---
+++ basicStrings.cpp
@@ -0,0 +1,26 @@
+﻿#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+
+https://www.hackerrank.com/challenges/c-tutorial-strings/problem
+
+int main() {
+	std::string a;
+	std::string b;
+	char temp;
+
+	std::cin >> a;
+	std::cin >> b;
+	std::cout << a.size() << " " << b.size() << std::endl;
+	std::cout << a + b << std::endl;
+	temp = b[0];
+	b[0] = a[0];
+	a[0] = temp;
+	std::cout << a << " " << b;
+
+	return 0;
+}
